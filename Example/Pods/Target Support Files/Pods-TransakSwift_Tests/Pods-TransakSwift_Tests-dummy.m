fa28c08beeaf9d739f5ac76290f49125263529d4
Chung Tran <bigearsenal@gmail.com>
2021-07-27 10:33:55 +0700

    remove ignored files

--- Example/Pods/Target Support Files/Pods-TransakSwift_Tests/Pods-TransakSwift_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TransakSwift_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_TransakSwift_Tests
-@end
--- Example/Pods/Target Support Files/TransakSwift/TransakSwift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_TransakSwift : NSObject
-@end
-@implementation PodsDummy_TransakSwift
-@end
