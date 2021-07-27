133aa5e0bf1278b9dd0aa2c8815c857c5eed10ae
Chung Tran <bigearsenal@gmail.com>
2021-07-27 10:32:19 +0700

    Add dependencies

--- Example/Pods/Target Support Files/Pods-TransakSwift_Example/Pods-TransakSwift_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-TransakSwift_Example/Pods-TransakSwift_Example-frameworks.sh
@@ -175,9 +175,13 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/BEPureLayout/BEPureLayout.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/PureLayout/PureLayout.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/TransakSwift/TransakSwift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/BEPureLayout/BEPureLayout.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/PureLayout/PureLayout.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/TransakSwift/TransakSwift.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
---
+++ TransakSwift/Classes/TransakViewController.swift
@@ -0,0 +1,11 @@
+//
+//  TransakViewController.swift
+//  FBSnapshotTestCase
+//
+//  Created by Chung Tran on 27/07/2021.
+//
+
+import WebKit
+open class TransakViewController: UIViewController {
+    
+}
